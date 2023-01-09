26d9e94a439bab6cdf874be8e175a1e5b025d680
1228275753 <wangjin122@qq.com>
2023-01-09 11:29:53 +0800

    更换ruoyi

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
-# vue-blok
+## 开发
 
-## Project setup
-```
-yarn install
-```
+```bash
+# 克隆项目
+git clone https://gitee.com/y_project/RuoYi-Vue
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+# 进入项目目录
+cd ruoyi-ui
 
-### Compiles and minifies for production
-```
-yarn build
-```
+# 安装依赖
+npm install
 
-### Lints and fixes files
-```
-yarn lint
+# 建议不要直接使用 cnpm 安装依赖，会有各种诡异的 bug。可以通过如下操作解决 npm 下载速度慢的问题
+npm install --registry=https://registry.npmmirror.com
+
+# 启动服务
+npm run dev
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+浏览器访问 http://localhost:80
+
+## 发布
+
+```bash
+# 构建测试环境
+npm run build:stage
+
+# 构建生产环境
+npm run build:prod
+```
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
+    <theme-picker />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ThemePicker from "@/components/ThemePicker";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { ThemePicker },
+    metaInfo() {
+        return {
+            title: this.$store.state.settings.dynamicTitle && this.$store.state.settings.title,
+            titleTemplate: title => {
+                return title ? `${title} - ${process.env.VUE_APP_TITLE}` : process.env.VUE_APP_TITLE
+            }
+        }
+    }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#app .theme-picker {
+  display: none;
 }
 </style>
